15a47cc6b4a5a2beb2745af1d65770116be2e6df
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2020-04-19 22:44:39 -0500

    Paginacion funcional y pagina de error creada

--- functions.php
+++ functions.php
@@ -27,6 +27,15 @@ function obtener_post($post_por_pagina, $conexion){
     return $sentencia -> fetchAll();
 }
 
+function numero_paginas($post_por_pagina, $conexion){
+    $total_post = $conexion -> prepare('SELECT FOUND_ROWS() as total');
+    $total_post -> execute();
+    $total_post = $total_post -> fetch()['total'];
+
+    $numero_paginas = ceil($total_post / $post_por_pagina);
+    return $numero_paginas;
+}
+
 function id_articulo($id){
     return (int)limpiarDatos($id);
 }
--- views/buscar.view.php
+++ views/buscar.view.php
@@ -1,7 +1,7 @@
 <?php require 'header.php'; ?>
 
     <div class="contenedor">
-        <?php foreach($posts as $post): ?>
+        <?php foreach($resultados as $post): ?>
             <div class="post">
                 <article>
                     <h2 class="titulo"><a href="single.php?id=<?php echo $post['id'] ?>"><?php echo $post['titulo'] ?></a></h2>
